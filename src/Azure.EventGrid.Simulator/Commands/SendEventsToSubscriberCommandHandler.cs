fdfeb518ec58ece227355d281adf55f4b82a8537
Hasmukh Patel <N17963@newday.co.uk>
2022-10-20 00:53:28 +0100

    working webhook

--- src/Azure.EventGrid.Simulator/Commands/SendEventsToSubscriberCommandHandler.cs
+++ src/Azure.EventGrid.Simulator/Commands/SendEventsToSubscriberCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using System.Text;
+using Azure.EventGrid.Simulator.Extensions;
+using Azure.EventGrid.Simulator.Models;
+using Azure.EventGrid.Simulator.Settings;
+using MediatR;
 using Newtonsoft.Json;
 
 namespace Azure.EventGrid.Simulator.Commands;
@@ -9,13 +13,15 @@ public class SendEventsToSubscriberCommandHandler : AsyncRequestHandler<SendEven
     private readonly ILogger<SendEventsToSubscriberCommandHandler> _logger;
     private const string _validationHandshakeEventType = "Microsoft.EventGrid.SubscriptionValidationEvent";
 
-    public SendEventsToSubscriberCommandHandler(IHttpClientFactory httpClientFactory, ILogger<SendEventsToSubscriberCommandHandler> logger)
+    public SendEventsToSubscriberCommandHandler(IHttpClientFactory httpClientFactory,
+        ILogger<SendEventsToSubscriberCommandHandler> logger)
     {
         _httpClientFactory = httpClientFactory;
         _logger = logger;
     }
     protected override async Task Handle(SendEventsToSubscriberCommand request, CancellationToken cancellationToken)
     {
+        string topicName = request.Topic.Name;
         foreach (var @eventGridEvent in request.Events)
         {
             try
@@ -35,6 +41,33 @@ public class SendEventsToSubscriberCommandHandler : AsyncRequestHandler<SendEven
                         //return result;
                     }
                 }
+                else
+                {
+                    foreach (var subscription in request.Topic.Subscribers)
+                    {
+                        if (subscription.Filter.AcceptsEvent(@eventGridEvent))
+                        {
+                            var json = JsonConvert.SerializeObject(new[] { @eventGridEvent }, Formatting.Indented);
+                            using var content = new StringContent(json, Encoding.UTF8, "application/json");
+                            var httpClient = _httpClientFactory.CreateClient();
+                            httpClient.DefaultRequestHeaders.Add(Constants.AegEventTypeHeader, Constants.NotificationEventType);
+                            httpClient.DefaultRequestHeaders.Add(Constants.AegSubscriptionNameHeader, subscription.Name.ToUpperInvariant());
+                            httpClient.DefaultRequestHeaders.Add(Constants.AegDataVersionHeader, @eventGridEvent.DataVersion);
+                            httpClient.DefaultRequestHeaders.Add(Constants.AegMetadataVersionHeader, @eventGridEvent.MetadataVersion);
+                            httpClient.DefaultRequestHeaders.Add(Constants.AegDeliveryCountHeader, "0"); // TODO implement re-tries
+                            httpClient.Timeout = TimeSpan.FromSeconds(60);
+
+                            await httpClient.PostAsync(subscription.Endpoint, content)
+                                .ContinueWith(t => LogResult(t, @eventGridEvent, subscription, topicName));
+                        }
+                        else
+                        {
+                            _logger.LogDebug("Event {EventId} filtered out for subscriber '{SubscriberName}'", @eventGridEvent.Id, subscription.Name);
+                        }
+                    }
+
+
+                }
             }
             catch (Exception e)
             {
@@ -42,5 +75,21 @@ public class SendEventsToSubscriberCommandHandler : AsyncRequestHandler<SendEven
             }
         }
         throw new NotImplementedException();
+    }
+    private void LogResult(Task<HttpResponseMessage> task, EventGridEvent evt, SubscriptionSettings subscription, string topicName)
+    {
+        if (task.IsCompletedSuccessfully && task.Result.IsSuccessStatusCode)
+        {
+            _logger.LogDebug("Event {EventId} sent to subscriber '{SubscriberName}' on topic '{TopicName}' successfully", evt.Id, subscription.Name, topicName);
+        }
+        else
+        {
+            _logger.LogError(task.Exception?.GetBaseException(),
+                "Failed to send event {EventId} to subscriber '{SubscriberName}', '{TaskStatus}', '{Reason}'",
+                evt.Id,
+                subscription.Name,
+                task.Status.ToString(),
+                task.Result?.ReasonPhrase);
+        }
     }
 }
--- src/Azure.EventGrid.Simulator/Program.cs
+++ src/Azure.EventGrid.Simulator/Program.cs
@@ -4,6 +4,7 @@ using Azure.EventGrid.Simulator.Settings;
 using MediatR;
 using Newtonsoft.Json;
 using System.Reflection;
+using Azure.EventGrid.Simulator.Commands;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,7 +23,8 @@ app.MapPost("/api/events",
     async (HttpContext context, IMediator mediator, SimulatorSettings settings) =>
 {
     var requestBody = await context.RequestBody();
+    var topic = settings.Topics.First(x => x.Port == context.Request.Host.Port);
     var events = JsonConvert.DeserializeObject<EventGridEvent[]>(requestBody);
-
+    var resposne = await mediator.Send(new SendEventsToSubscriberCommand(events, topic));
 });
 app.Run();
