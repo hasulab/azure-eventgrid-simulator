fad10b720a3f6faaacc5c7be6021c5c47af9cee0
hasmukhlalpatel <hasmukhlalpatel@live.co.uk>
2022-12-03 20:31:10 +0000

    moved services into Extensions

---
+++ src/Azure.EventGrid.Simulator/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Azure.EventGrid.Simulator.Services;
+using Azure.EventGrid.Simulator.Settings;
+using MediatR;
+using System.Reflection;
+
+namespace Azure.EventGrid.Simulator.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {   public static IServiceCollection AddSimulatorServices(this IServiceCollection services, IConfiguration configuration)
+		{
+            var settings = new SimulatorSettings();
+            configuration.Bind(settings);
+
+            services.AddSingleton(_ => settings);
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddHttpClient();
+			services.Configure<EventDeliverySettings>(opt
+				=> configuration.GetSection("EventDeliverySettings")
+					.Bind(opt));
+            services.AddHostedService<EventDeliveryService>();
+            services.AddSingleton<IEventQueueService, EventQueueService>();
+
+            return services;
+		}
+    }
+}
--- src/Azure.EventGrid.Simulator/Program.cs
+++ src/Azure.EventGrid.Simulator/Program.cs
@@ -10,29 +10,9 @@ using Azure.EventGrid.Simulator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var settings = new SimulatorSettings();
-builder.Configuration.Bind(settings);
-
-builder.Services.AddSingleton(_ => settings);
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddHttpClient();
-builder.Services.Configure<EventDeliverySettings>(opt
-    => builder.Configuration.GetSection("EventDeliverySettings")
-        .Bind(opt));
-builder.Services.AddHostedService<EventDeliveryService>();
-
-builder.Services.AddSingleton<IEventQueueService, EventQueueService>();
-
+builder.Services.AddSimulatorServices(builder.Configuration);
 var app = builder.Build();
 
-
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/api/events", 
-    async (HttpContext context, IMediator mediator, SimulatorSettings settings) =>
-{
-    var requestBody = await context.RequestBody();
-    var topic = settings.Topics.First(x => x.Port == context.Request.Host.Port);
-    var events = JsonConvert.DeserializeObject<EventGridEvent[]>(requestBody);
-    var resposne = await mediator.Send(new SendEventsToSubscriberCommand(events, topic));
-});
+app.MapSimulatorEndpoint();
 app.Run();
